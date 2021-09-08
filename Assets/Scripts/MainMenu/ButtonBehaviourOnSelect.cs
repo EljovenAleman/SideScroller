ee08acc303e897764012fffbb40875dc9c2fb0b6
EljovenAleman <diegobettendorf@gmail.com>
2021-09-08 14:11:12 -0300

    added textures to buttons

---
+++ Assets/Scripts/MainMenu/ButtonBehaviourOnSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviourOnSelect : MonoBehaviour
+{
+    [SerializeField] Button button;
+    //[SerializeField] AudioSource myAudio;
+
+
+
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    void OnButtonClick()
+    {
+        button.image.color = new Color(255, 0, 0);
+        //myAudio.Play();
+    }
+}
